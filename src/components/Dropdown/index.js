c8a7aa35af4083c7c7601c8a10fe5b0a49568d96
Matheus Alves <matheusalv.oliveira@gmail.com>
2023-07-18 00:55:16 -0300

    dropdown style

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -3,14 +3,13 @@ import './Dropdown.css'
 const Dropdown = ( props ) => {
     return (
 
-        <div>
+        <div className="dropdown">
             <label>
             {props.label}
             </label>
 
             <select>
-                {props.itens.map( 
-                    item =>  <option>{item}</option> )}
+                {props.itens.map( item =>  <option key={item}>{item}</option> )}
 
             </select>
         </div>
